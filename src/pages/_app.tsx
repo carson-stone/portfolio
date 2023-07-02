52380491a49839457cf5979cd719aea294514166
Carson Stone <carson@landis.com>
2023-07-02 14:47:26 -0400

    setup and configuration

---
+++ src/pages/_app.tsx
@@ -0,0 +1,30 @@
+import type { AppProps } from 'next/app'
+
+import Head from 'next/head'
+import { inter } from "@/utils/fonts"
+
+import '@/styles/globals.css'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      {/* global font */}
+      <style
+        jsx
+        global
+      >
+        {`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}
+      </style>
+
+      <Head>
+        <title>Carson Stone</title>
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
+}
